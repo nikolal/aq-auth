bc1a59ed24de99abea4e9c588861b14f4c24542f
nikolal <iamnikolalukic@hotmail.com>
2017-05-27 20:22:00 +0200

    Register Screen

--- aqauth/index.android.js
+++ aqauth/index.android.js
@@ -4,50 +4,31 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
+
+import Register from './src/components/register/Register.js'
 
 export default class aqauth extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Register />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginTop: 54,
+    flex: 1
+  }
+})
 
-AppRegistry.registerComponent('aqauth', () => aqauth);
+AppRegistry.registerComponent('aqauth', () => aqauth)
---
+++ aqauth/src/components/register/Register.js
@@ -0,0 +1,114 @@
+import React, { Component, propTypes } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Dimensions,
+  Button,
+  TouchableOpacity
+} from 'react-native'
+
+const Register = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('./background.jpeg')}
+        resizeMode='cover'
+      />
+      <View style={styles.containerText}>
+        <Text style={styles.textRegister}>Register</Text>
+        <Text style={styles.descriptionext}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          Eum delectus incidunt at, cum, hic sed soluta saepe ut exercitationem earum
+        </Text>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          autoCorerrect={false}
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          autoCorerrect={false}
+          autoCapitalize="none"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+        <View style={styles.bottomTextContainer}>
+          <TouchableOpacity>
+            <Text style={styles.bottomText}>Go to Login</Text>
+          </TouchableOpacity>
+          <Text style={styles.bottomText}>|</Text>
+          <TouchableOpacity>
+            <Text style={styles.bottomText}>Forgot Pasword</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
+}
+export default Register
+
+const {width, height} = Dimensions.get('window')
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    backgroundColor: 'purple',
+    opacity: 0.8,
+  },
+  containerText: {
+    justifyContent: 'space-around',
+    width: width,
+    height: height,
+    position: 'absolute',
+    paddingVertical: 80,
+    paddingHorizontal: 30,
+    paddingBottom: 150
+  },
+  textRegister: {
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+  descriptionext: {
+    alignSelf: 'center',
+    color: 'white',
+    fontSize: 15
+  },
+  input: {
+    height: 40,
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 20,
+    color: 'white',
+    borderRadius: 50
+  },
+  button: {
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 50,
+    height: 40,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center'
+  },
+  bottomTextContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  bottomText: {
+    color: 'white',
+    fontSize: 15
+  }
+})
